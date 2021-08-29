f1a129ae863ee59f7b74bee48330790f5999620f
NoobieUs37 <NoobieUs37@protonmail.com>
2021-08-29 12:16:04 +0530

    Initial commit

---
+++ build/precache-manifest.db3a4cf62099571bedc8a1156c89f16e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82fdb6cbc1cdfbdea80f9d601195679a",
+    "url": "/thirukural/index.html"
+  },
+  {
+    "revision": "3195daf04648aac7cefc",
+    "url": "/thirukural/static/js/2.52567a79.chunk.js"
+  },
+  {
+    "revision": "f13f09d5a5911dd2bfb9",
+    "url": "/thirukural/static/js/main.bab77a5a.chunk.js"
+  },
+  {
+    "revision": "1793000fb27f61fb5835",
+    "url": "/thirukural/static/js/runtime-main.d669087a.js"
+  }
+]);
